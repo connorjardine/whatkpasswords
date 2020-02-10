e11818c82390f30d19564cbaa9b05c294827bfc0
Connor Jardine <2188232j@student.gla.ac.uk>
2020-02-10 11:15:24 +0000

    started first route.

--- blueprints/login.py
+++ blueprints/login.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 
-login = Blueprint('login', __name__)
+login = Blueprint('login', __name__, template_folder='templates')
 
 passwords = ['cat', 'dog', 'house', 'boat', 'golf', 'mike', 'hotel', 'sierra', 'tango', 'blue']
 
 
 @login.route('/')
 def login_blueprint():
-    return "whatkpasswords"
+    return render_template('login.html', pw=passwords)
