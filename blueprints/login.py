f656f9c60c52a0610d50d2ef3e162de4b56fc420
Connor Jardine <2188232j@student.gla.ac.uk>
2020-02-10 12:00:04 +0000

    grid ready

--- blueprints/login.py
+++ blueprints/login.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template
 
 login = Blueprint('login', __name__, template_folder='templates')
 
-passwords = ['cat', 'dog', 'house', 'boat', 'golf', 'mike', 'hotel', 'sierra', 'tango', 'blue']
 
 
 @login.route('/')
 def login_blueprint():
+    passwords = ['cat', 'dog', 'house', 'boat', 'golf', 'mike', 'hotel', 'sierra', 'tango', 'blue']
     return render_template('login.html', pw=passwords)
+
